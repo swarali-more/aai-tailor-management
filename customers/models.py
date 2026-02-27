eda417ef172510fee83585049b83481eaf868bad
Swarali <swaralimore3@gmail.com>
2026-02-27 23:26:44 +0530

    Added blouse model linked to customer

--- customers/models.py
+++ customers/models.py
@@ -8,3 +8,7 @@ class Customer(models.Model):
 
     def __str__(self):
         return self.name
+    
+class Blouse(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    blouse_name = models.CharField(max_length=100)
