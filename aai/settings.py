0603d2a8b26d14da963c1284b96fc970d5d6e970
Swarali <swaralimore3@gmail.com>
2026-02-28 16:08:13 +0530

    changed width area which is portable for all devices

--- aai/settings.py
+++ aai/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-)0@k59$q((=qpqp+&e2xnal36=loro*aatu%7k4@fpjlx57uyl
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['10.245.1.33']
 
 
 # Application definition
